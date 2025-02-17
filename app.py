5cb41d58b713ac64e0ea8e38cbae92f59ea0158c
SamiUllah0090 <sr823600@gmail.com>
2025-02-17 05:01:34 -0800

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello World!  Usman kia hall hain kase ho tumhri dost kasi hai abbay wali jisko tum dekhte rehte lecture k time Running and Fucking Running........!!! I\'m host %s' % socket.gethostname()
+    return 'Hello World!   Running and Fucking Running........!!! I\'m host %s' % socket.gethostname()
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
